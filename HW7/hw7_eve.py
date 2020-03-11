8c301bd5da109b8ab812e198c0fcc2363f49401a
evelynjulia <evewicksteed@gmail.com>
2020-03-11 12:33:13 -0700

    finished q3

--- HW7/hw7_eve.py
+++ HW7/hw7_eve.py
@@ -11,8 +11,17 @@ import datetime as dt
 from scipy import interpolate
 import pandas as pd
 import math 
+from pathlib import Path
+import os
 
-fig_dir = '/Users/ewicksteed/Documents/Eve/NWP/HW7/'
+current_dir = os.getcwd()
+fig_dir = str(current_dir)+'/HW7/'
+
+
+#file_to_open = data_folder / "raw_data.txt"
+
+
+#fig_dir = '/Users/ewicksteed/Documents/Eve/NWP/HW7/'
 
 # fig_dir = '/Users/catherinemathews/UBC/NWP'
 
@@ -47,20 +56,48 @@ conc[150:201] = np.linspace(c_max, 0, 51)        # insert right side of triangle
 conc[20:41] = np.linspace(0, -0.5*c_max, 21)    # insert left side of triangle
 conc[40:61] = np.linspace(-0.5*c_max, 0, 21)    # insert right side of triangle
 
+# %% 3) show (in red) the ideal exact final solution
+
+#Also, on the same plot, show (in red) the ideal exact final solution,
+# after the puff anomaly has been advected downwind, as given by
+
+c_ideal = np.zeros(max_x) # initial concentration of ideal background is zero
+
+#c_max = 10.0                      # max initial concentration
+
+c_ideal[800:851] = np.linspace(0, c_max, 51)    # insert left side of triangle
+c_ideal[850:901] = np.linspace(c_max, 0, 51)    # insert right side of triangle
+c_ideal[720:741] = np.linspace(0, -0.5*c_max, 21)    # insert left side of triangle
+c_ideal[740:761] = np.linspace(-0.5*c_max, 0, 21)    # insert right side of triangle
+
 
 # %% 2b) Plot (using blue colour) the initial concentration distribution on a graph.
 
 xvals = np.arange(0,1000,1)
 
-plt.plot(xvals,conc)
-plt.show()
+# plt.plot(xvals,conc)
+# plt.show()
 
 fig, ax = plt.subplots(1,1, figsize=myfigsize)
-plt.plot(xvals,conc)
+ax.plot(xvals,conc, color = 'b', label = 'original concentration')
+ax.plot(xvals,c_ideal, color='r', label = 'exact final solution')
 ax.set_xlabel("Grid index (i)")
 ax.set_ylabel("Quantity")
 #plt.title("")
 # ax.set_ylim(-5,95)
-plt.show()
-#plt.savefig(fig_dir+run_date+'_'+'example_plot'+'.png')
+plt.legend()
+#plt.show()
+plt.savefig(fig_dir+run_date+'_'+'example_plot'+'.png')
+
+
+
+# %% 4) Advect the concentration puff anomaly for the following number of time steps
+
+
+nsteps = (max_x - 300) / (u * dt / dx)
+# and plot (in green) the resulting concentration on the same graph, using ...
+
+#  "forward in time, backward in space" (FTBS) .
+#              [See a sample of this output below.]
+
 
