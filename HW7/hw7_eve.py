5dd006593002f845a94b83751aec238cc9edf14f
evelynjulia <evewicksteed@gmail.com>
2020-03-23 11:28:53 -0700

    fixed RK3

--- HW7/hw7_eve.py
+++ HW7/hw7_eve.py
@@ -143,88 +143,6 @@ iters = np.arange(0,nsteps,1)
 
 ### RK3 
 
-########################
-
-mat = np.empty((int(nsteps), int(max_x)))
-
-mat[0,:] = conc.copy()
-
-
-for n in range(mat.shape[0]-1): #time steps
-    print(n)
-    for j in range(3, mat.shape[1]-3): # spatial
-        mat[n+1, j+1] = mat[n,j] + 
-        (-1/2) * Cr * (mat[n,j+1] - mat[n,j]) 
-        + (Cr*Cr/8) * (mat[n,j+2] + mat[n,j-2] - 2*mat[n,j]) 
-        - (Cr*Cr*Cr/48) * ( mat[n,j+3] - 3*mat[n,j+1] + 3*mat[n,j-1] - mat[n,j-3] )
-
-
-plt.plot(mat[0,:])
-plt.plot(mat[-1,:])
-plt.show()
-
-#    mat[1,j] = c_now_RK3 + -(1/2) * Cr * (cjp1 - c_now_RK3) + (Cr*Cr/8) * (cjp2 + cjm2 - 2*c_now_RK3) - (Cr*Cr*Cr/48) * ( cjp3 - 3*cjp1 + 3*cjm1 - cjm3 )
-    
- #   cjm3
- #   cjm2
-    print(j)
-
-########################
-
-
-c_now_RK3 = conc.copy()
-cjm3 = shift(c_now_RK3, -3, cval=0)
-cjm2 = shift(c_now_RK3, -2, cval=0)
-cjm1 = shift(c_now_RK3, -1, cval=0)
-cjp1 = shift(c_now_RK3, 1, cval=0)
-cjp2 = shift(c_now_RK3, 2, cval=0)
-cjp3 = shift(c_now_RK3, 3, cval=0)
-c_next_RK3 = c_now_RK3 + -(1/2) * Cr * (cjp1 - c_now_RK3) + (Cr*Cr/8) * (cjp2 + cjm2 - 2*c_now_RK3) - (Cr*Cr*Cr/48) * ( cjp3 - 3*cjp1 + 3*cjm1 - cjm3 )
-
-
-plt.plot(c_now_RK3, label="now")
-plt.plot((cjm3), label = 'minus 3')
-
-plt.plot(c_now_RK3, label = 'now ')
-plt.plot(c_next_RK3, label = 'next')
-plt.legend()
-plt.show()
-
-
-c_now_RK3 = conc.copy()
-for time_step in iters:
-    cjm3 = shift(c_now_RK3, -3, cval=0)
-    cjm2 = shift(c_now_RK3, -2, cval=0)
-    cjm1 = shift(c_now_RK3, -1, cval=0)
-    cjp1 = shift(c_now_RK3, 1, cval=0)
-    cjp2 = shift(c_now_RK3, 2, cval=0)
-    cjp3 = shift(c_now_RK3, 3, cval=0)
-    T_tendency_j = -(1/2) * Cr * (cjp1 - c_now_RK3) + (Cr*Cr/8) * (cjp2 + cjm2 - 2*c_now_RK3) - (Cr*Cr*Cr/48) * ( cjp3 - 3*cjp1 + 3*cjm1 - cjm3 )
-    c_next_RK3 = c_now_RK3 + T_tendency_j 
-    c_now_RK3 = c_next_RK3.copy()
-
-
-c_RK3 = c_now_RK3.copy()
-
-#####################
-# 23 march
-
-c_now_RK3 = conc.copy()
-
-big_matrix = np.empty((int(nsteps), int(max_x)))
-big_matrix[0,:] = c_now_RK3
-
-for time_step in range(int(nsteps)-1):
-    tjn = big_matrix[time_step,:]
-    term1 = -(1/2) * Cr * (np.roll(tjn,-1) - tjn)
-    term2 = (Cr*Cr/8) * (np.roll(tjn,-2) + np.roll(tjn,+2) - 2*tjn)
-    term3 = (Cr*Cr*Cr/48) * (np.roll(tjn,-3) - 3*np.roll(tjn,-1) + 3*np.roll(tjn,+1) - np.roll(tjn,+3))
-    tend = term1 + term2 - term3
-    tjnp1 = tjn + tend
-    big_matrix[time_step+1,:] = tjnp1
-
-
-### next attempts
 
 c_now_RK3 = conc.copy()
 
@@ -240,40 +158,21 @@ for time_step in range(int(nsteps)-1):
     big_matrix[time_step+1,:] = t1 - t2 + t3 - t4
 
 
-
-
-
-
-
-
-
-# ### previou version
-# c_now_RK3 = conc.copy()
-
-# for time_step in iters:
-#     #print(time_step)
-#     d_conc1 = shift(c_now_RK3, 1, cval=0) - shift(c_now_RK3, -1, cval=0) # c(j+1) - c(j-1)
-#     d_conc2 = shift(c_now_RK3, 2, cval=0) - shift(c_now_RK3, -2, cval=0) # c(j+2) - c(j-2)
-#     d_conc3 = shift(c_now_RK3, 3, cval=0) - shift(c_now_RK3, -3, cval=0) # c(j+3) - c(j-3)
-#     c_next_RK3 = ( (1- (Cr**2)/4)*c_now_RK3 ) - ( ((Cr/2)- (3*(Cr**3)/48)) * (d_conc1) ) + ( ((Cr**2)/8) * (d_conc2) ) - ( ((Cr**3)/48) * (d_conc3) )
-#     c_now_RK3 = c_next_RK3.copy()
-
-
-# c_RK3 = c_now_RK3.copy()
+c_RK3 = big_matrix[-1,:]
 
 
 # %% Plot RK3 
 
 fig, ax = plt.subplots(1,1, figsize=myfigsize)
-ax.plot(xvals, c_RK3, color='g', label = 'RK3 solution')
 ax.plot(xvals,conc, color = 'b', label = 'original concentration')
 ax.plot(xvals,c_ideal, color='r', label = 'exact final solution')
+ax.plot(xvals, c_RK3, color='g', label = 'RK3 solution')
 ax.set_xlabel("Grid index (i)")
 ax.set_ylabel("Quantity")
 #plt.title("")
 # ax.set_ylim(-5,95)
 plt.legend()
-plt.show()
+#plt.show()
 plt.savefig(fig_dir+'RK3'+'_'+run_date+'.png')
 
 
