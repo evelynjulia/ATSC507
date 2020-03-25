bd582edf9f095f0a8fed877f2ec118fed0042f9a
evelynjulia <evewicksteed@gmail.com>
2020-03-25 13:58:50 -0700

    plots a box

--- HW9/hw9.py
+++ HW9/hw9.py
@@ -13,6 +13,7 @@ import pandas as pd
 import math 
 from pathlib import Path
 import os
+from mpl_toolkits.mplot3d import Axes3D
 
 current_dir = os.getcwd()
 fig_dir = str(current_dir)+'/HW9/'
@@ -36,41 +37,29 @@ x2d = np.ones((c,c)) *x
 y2d = np.ones((c,c)) *y
 z2d = np.ones((c,c)) *z
 
-xv, yv = np.meshgrid(x, y)
+top =  np.ones((c,c)) *2
+bottom =  np.ones((c,c)) *-2
 
+#xv, yv, zv = np.meshgrid(x, y, z)
+
+xv, yv = np.meshgrid(x, y)
+### #but don't want the inside points
+zv2 = np.meshgrid(z,z)
 
 # %%
 # plot
 
-
-from mpl_toolkits.mplot3d import Axes3D
-
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-#ax.scatter(xv, yv, zv)
-#ax.plot_wireframe(xv, yv, z2)
-ax.contour3D(x2d, y2d, z2d, 50, cmap='binary')
-#ax.plot_trisurf(x2d, y2d, z2d, linewidth=0, antialiased=False)
-#ax.contour3D(x2d,y2d,z2d)
+#ax.scatter(xv, yv, z)
+ax.plot_wireframe(xv, yv, top, color = 'b')
+ax.plot_wireframe(xv, yv, bottom, color = 'b')
+ax.plot_wireframe(top, xv, yv, color = 'r')
+ax.plot_wireframe(bottom, xv, yv, color = 'r')
+ax.plot_wireframe(xv, top, yv, color = 'g')
+ax.plot_wireframe(xv, bottom, yv, color = 'g')
 #ax.view_init(60, 35)
-fig
-plt.show()
+#plt.show()
+plt.savefig(fig_dir+'step1_box'+'_'+run_date+'.png')
 
 # %%
-# test plot
-
-def f(x, y):
-    return np.sin(np.sqrt(x ** 2 + y ** 2))
-
-x = np.linspace(-6, 6, 30)
-y = np.linspace(-6, 6, 30)
-
-X, Y = np.meshgrid(x, y)
-Z = f(X, Y)
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.contour3D(X, Y, Z, 50, cmap='binary')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-
