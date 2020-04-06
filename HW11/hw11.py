a87efa4b8c788a26c1403e659d72a6c4d8c95b7b
evelynjulia <evewicksteed@gmail.com>
2020-04-06 11:43:30 -0700

    just need to do plots

--- HW11/hw11.py
+++ HW11/hw11.py
@@ -8,10 +8,13 @@
 # %% load libraries
 
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 
 # %% Set up data
+
+# Each field (i.e., each weather map) below covers an
+# area from North to South and West to East.
 
 anal = np.array([[5.2, 5.3, 5.4, 5.3],[5.3, 5.4, 5.5, 5.4],[5.4, 5.5, 5.6, 5.5],[5.5, 5.6, 5.7, 5.6],[5.6, 5.7, 5.8, 5.7]])
 
@@ -21,5 +24,79 @@ verif = np.array([[5.3, 5.3, 5.3, 5.4],[5.4, 5.3, 5.4, 5.5],[5.5, 5.4, 5.5, 5.5]
 
 clim = np.array([[5.4, 5.4, 5.4, 5.4],[5.4, 5.4, 5.4, 5.4],[5.5, 5.5, 5.5, 5.5],[5.6, 5.6, 5.6, 5.6],[5.7, 5.7, 5.7, 5.7]])
 
+# %% create error functions
 
-# %%
+
+
+# %% a. Mean forecast error
+
+ME_fcst = np.mean(fcst-verif)
+
+print("the mean forecast error is", round(ME_fcst, 3))
+
+# %% b. mean persistence error
+
+ME_persist = np.mean(anal-verif)
+
+print("the mean persistence error is", round(ME_persist, 3))
+
+# %% c. mean absolute forecast error
+
+MAE_fcst = np.mean(np.abs(fcst-verif))
+
+print("the mean absolute forecast error is", round(MAE_fcst, 3))
+
+# %% d. mean squared forecast error
+
+MSE_fcst = np.mean((fcst-verif)**2)
+
+print("the mean squared forecast error is", round(MSE_fcst, 3))
+
+# %% e. mean squared climatology error
+
+MSE_clim = np.mean((clim-verif)**2)
+
+print("the mean squared forecast error is", round(MSE_clim, 3))
+
+
+# %% f. mean squared forecast error skill score
+
+MSE_ref = MSE_clim
+MSESS_fcst = 1 - (MSE_fcst/MSE_ref)
+
+print("the mean squared forecast error skill score is", round(MSESS_fcst, 3))
+
+
+# %% g. RMS forecast error
+
+RMSE_fcst = np.sqrt(MSE_fcst)
+
+print("the root mean squared forecast error is", round(RMSE_fcst, 3))
+
+
+# %% h. correlation coefficient between forecast and verification
+
+fprime = fcst - np.mean(fcst)
+vprime = verif - np.mean(verif)
+
+PCC_fv = (np.mean(fprime*vprime)) / ( (np.sqrt(np.mean(fprime**2))) * np.sqrt(np.mean(vprime**2)) )
+
+print("the Pearson correlation coefficient between forecast and verification is", round(PCC_fv, 3))
+
+# %% i. forecast anomaly correlation
+
+AC = np.mean( ((fcst - clim)- np.mean(fcst - clim)) * ((verif - clim)- np.mean(verif - clim)) )  / np.sqrt( np.mean(((fcst - clim) - np.mean(fcst - clim))**2) * np.mean(((verif - clim) - np.mean(verif - clim))**2) )
+
+print("the forecast anomaly correlation is", round(AC,3))
+
+# %% j. persistence anomaly correlation
+
+AC_persist = np.mean( ((anal - clim)- np.mean(anal - clim)) * ((verif - clim)- np.mean(verif - clim)) )  / np.sqrt( np.mean(((anal - clim) - np.mean(anal - clim))**2) * np.mean(((verif - clim) - np.mean(verif - clim))**2) )
+
+print("the persistance anomaly correlation is", round(AC_persist,3))
+
+
+# %% k. Draw height contours by hand for each field, to show locations of ridges and troughs.
+
+plt.contour(anal)
+plt.show()
