f0c4d7aa4f49cee86ef269c19ab426e4ad35d0fd
evelynjulia <evewicksteed@gmail.com>
2020-04-09 19:23:12 -0700

    part a q3 done

--- HW10/hw10.py
+++ HW10/hw10.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+import math
 
 current_dir = os.getcwd()
 out_dir = str(current_dir)+'/HW10/'
@@ -60,46 +61,44 @@ def gaussInt(a,b,wk,fxk):
     return I
 
 
+def get_area(a,b,m):
+
+    # define weights and zero values
+    if m==2:
+        ek = np.array([0.5773502692, -0.5773502692])
+        w = np.array([1,1])
+    elif m==4:
+        ek = np.array([0.3399810436, -0.3399810436, 0.8611363116, -0.8611363116])
+        w = np.array([0.6521451549, 0.6521451549, 0.3478548451, 0.3478548451])
+    elif m==6:
+        ek = np.array([0.2386191861, -0.2386191861, 0.6612093865, -0.6612093865, 0.9324695142, -0.9324695142])
+        w = np.array([0.4679139346, 0.4679139346, 0.3607615730, 0.3607615730, 0.1713244924, 0.1713244924])
+    elif m==8:
+        ek = np.array([0.1834346425, -0.1834346425, 0.5255324099, -0.5255324099, 0.7966664774, -0.7966664774, 0.9602898565, -0.9602898565])
+        w = np.array([0.3626837834, 0.3626837834, 0.3137066459, 0.3137066459, 0.2223810345, 0.2223810345, 0.1012285363, 0.1012285363])
+
+    # get xk values
+    xk = get_xk(a,b,ek)
+
+    # then get the function values, fxk:
+    fxk = func(xk)
+
+    # then integrate to get area:
+    area = gaussInt(a,b,w,fxk)
+
+    return area
+
+
+
+
 # integrate between 0 and 20
 a = 0
 b = 20
 
-
-# Get the weights
-#(i) 2 ,  (ii) 4 , (iii) 6 , (iv) 8 
-# M2 weights:
-# Ek = +- 0.5773502692
-# Wk = 1
-ek2 = np.array([0.5773502692, -0.5773502692])
-w2 = np.array([1,1])
-
-ek4 = np.array([0.3399810436, -0.3399810436, 0.8611363116, -0.8611363116])
-w4 = np.array([0.6521451549, 0.6521451549, 0.3478548451, 0.3478548451])
-
-ek6 = np.array([0.2386191861, -0.2386191861, 0.6612093865, -0.6612093865, 0.9324695142, -0.9324695142])
-w6 = np.array([0.4679139346, 0.4679139346, 0.3607615730, 0.3607615730, 0.1713244924, 0.1713244924])
-
-ek8 = np.array([0.1834346425, -0.1834346425, 0.5255324099, -0.5255324099, 0.7966664774, -0.7966664774, 0.9602898565, -0.9602898565])
-w8 = np.array([0.3626837834, 0.3626837834, 0.3137066459, 0.3137066459, 0.2223810345, 0.2223810345, 0.1012285363, 0.1012285363])
-
-
-# get xk values
-xk_m2 = get_xk(a,b,ek2)
-xk_m4 = get_xk(a,b,ek4)
-xk_m6 = get_xk(a,b,ek6)
-xk_m8 = get_xk(a,b,ek8)
-
-# then get the function values, fxk:
-fxk_m2 = func(xk_m2)
-fxk_m4 = func(xk_m4)
-fxk_m6 = func(xk_m6)
-fxk_m8 = func(xk_m8)
-
-# then integrate:
-area_m2 = gaussInt(a,b,w2,fxk_m2)
-area_m4 = gaussInt(a,b,w4,fxk_m4)
-area_m6 = gaussInt(a,b,w6,fxk_m6)
-area_m8 = gaussInt(a,b,w8,fxk_m8)
+area_m2 = get_area(a,b,m=2)
+area_m4 = get_area(a,b,m=4)
+area_m6 = get_area(a,b,m=6)
+area_m8 = get_area(a,b,m=8)
 
 
 data = {"area, m=2": [area_m2],
@@ -110,3 +109,62 @@ data = {"area, m=2": [area_m2],
 
 df = pd.DataFrame(data) 
 df.to_csv(out_dir+"area_from_gauss_quad.csv")
+
+
+# %% Question 3
+
+# Plot eq. (4.22) of Coiffier similar to his Fig 4.2, but for:  
+# (a) m=0 with n=1 to 4, 
+# (b) m=1 with n=1 to5, and 
+# (c) m=2 with n=2 to 6. 
+
+
+u = np.linspace(-1,1,100) # from -pi to pi
+
+n = 1
+m = 0
+
+
+
+
+def get_deriv(m,n,u):
+    if m == 0:
+        if n == 1:
+            ddu = 2*u
+        elif n== 2:
+            ddu = (12* u**2) -4
+        elif n==3:
+            ddu = 120*u**3 -72*u
+        elif n ==4:
+            ddu = 1680*u**4 - 1440*u**2 + 144
+    
+    return ddu
+
+
+u = np.linspace(-1,1,100)
+
+# start of for m = 0
+m = 0
+part_an = np.array([1,2,3,4])
+part_a = np.empty((len(part_an), len(u)))
+
+
+for n in part_an:
+    # calculate the function
+    t1 = np.sqrt((2*n +1)*(math.factorial(n-m)/math.factorial(n+m)))
+    t2 = ((1-u**2)**(m/2) ) / ((2**n) * math.factorial(n))
+    t3 = get_deriv(m,n,u)
+    Pmn =  t1 * t2 * t3 
+    # append answer to array
+    part_a[n-1] = Pmn
+
+plt.plot(u, part_a.T)
+plt.show()
+
+
+t1 = np.sqrt((2*n +1)*(math.factorial(n-m)/math.factorial(n+m)))
+t2 = ((1-u**2)**(m/2) ) / ((2**n) * math.factorial(n))
+t3 = get_deriv(m,n,u)
+
+
+Pmn =  t1 * t2 * t3 
