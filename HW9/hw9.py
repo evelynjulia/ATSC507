cc8d125ab85888ba58366caecdec4182e55c4a17
evelynjulia <evewicksteed@gmail.com>
2020-04-17 15:01:16 -0700

    have done q1

--- HW9/hw9.py
+++ HW9/hw9.py
@@ -31,3 +31,14 @@ gnomonic(radius=2, c=12, fig_dir=my_fig_dir, type='equiangular')
 
 # %% Question 1
 
+x = np.arange(-0.9, 1, 0.1)
+
+rho = 1
+k = int(len(x) / 3)  # the number of points in each region
+v1 = x[0:k+1]
+v2 = x[k:2*k+1]
+v3 = x[2*k:3*k+1]
+
+gen_p1 = round(sum(v1 * rho) / (1*k), 3)
+gen_p2 = round(sum(v2 * rho) / (1*k), 3)
+gen_p3 = round(sum(v3 * rho) / (1*k), 3)
