86408037721ee5e3dd41afa6af546397fe7c1a0c
evelynjulia <evewicksteed@gmail.com>
2020-04-18 18:37:24 -0700

    added label to plot title

--- HW9/hw9_functions.py
+++ HW9/hw9_functions.py
@@ -53,4 +53,4 @@ def gnomonic(radius, c, fig_dir, type='equidistant'):
     ax.plot_wireframe(bigx_top, bigZ_top, bigy_top, color = 'g')
     ax.plot_wireframe(bigx_bottom, bigZ_bottom, bigy_bottom, color = 'g')
     plt.title(type+' gnomonic projection')
-    plt.savefig(fig_dir+'test_'+type+'_gnomonic'+'_'+run_date+'.png')
+    plt.savefig(fig_dir+c+'c'+type+'_gnomonic'+'_'+run_date+'.png')
